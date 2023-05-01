3f31ec4ac90e44445d728e10afd0422eece6e047
BrianAbura <byaruhanga.abu@gmail.com>
2023-05-01 21:13:49 +0300

    New Files and Commits

--- about.php
+++ about.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Turbo Potato</title>
+    <title>Turbo Potato - About</title>
 </head>
 <body>
-    <h1>Welcome to Turbo Potato</h1>
-
-    <a href="about.php">About Us</a>
+    <h1>This the About us page</h1>
 </body>
 </html>
