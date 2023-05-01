a63909d07c16673008e0e4ccb36cf6cef1f97428
BrianAbura <byaruhanga.abu@gmail.com>
2023-05-01 21:10:35 +0300

    Turbo Potato Launch

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<?php 
-
-echo "This is the initial file created for turbo-potato";
-
-$name = "Changes from the VS CODE Again";
-
-echo $name;
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turbo Potato</title>
+</head>
+<body>
+    <h1>Welcome to Turbo Potato</h1>
+</body>
+</html>
